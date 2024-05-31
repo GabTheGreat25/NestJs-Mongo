0997e068720daa6106c5aa24efa9a8712921eebe
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-05-31 16:44:29 +0800

    add cors logic

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from "./app.module";
 import { AllExceptionsFilter } from "./utils/utils.exceptionsFilter";
 import ENV from "./config/config.environment";
 import { addSwagger } from "./utils";
+import { addCorsOptions } from "./config/config.corsOptions";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   addSwagger(app);
+  app.enableCors(addCorsOptions());
   app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(ENV.PORT);
 }
